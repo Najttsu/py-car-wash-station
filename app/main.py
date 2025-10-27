1f8c0fb333ebf8777342335660563da33caa781a
Bohdan Kutsyi <bodikoen@gmail.com>
2025-10-27 20:44:48 +0200

    Added if statement to wash_single_car function

--- app/main.py
+++ app/main.py
@@ -42,7 +42,8 @@ class CarWashStation:
         return round(wash_cost, 1)
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
         new_rate = self.average_rating * self.count_of_ratings + rate
